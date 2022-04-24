388566d8a23d4bcc18047150f9dccfe28c21ef72
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2022-04-24 12:47:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const SaphireClient = require("./Routes/classes/SaphireClient")
+const SaphireClient = require("./modules/classes/SaphireClient")
 const client = new SaphireClient()
 module.exports = client
-require('./Routes/functions/process')
-for (const file of ["command", "event"]) { require(`./src/structures/${file}`)(client) }
+require('./modules/functions/config/process')
+for (const file of ["command", "event"]) require(`./src/structures/${file}`)(client)
 client.start()
