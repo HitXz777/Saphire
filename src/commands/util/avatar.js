dd95fdb506694f88440fc13ce89ff01a5d401dcd
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2022-05-10 10:13:04 -0300

    Update avatar.js

--- src/commands/util/avatar.js
+++ src/commands/util/avatar.js
@@ -13,91 +13,163 @@ module.exports = {
     run: async (client, message, args, prefix, MessageEmbed, Database) => {
 
         let user = message.mentions.users.first() || message.mentions.repliedUser || client.users.cache.find(data => data.username?.toLowerCase() === args.join(' ')?.toLowerCase() || data.tag?.toLowerCase() === args[0]?.toLowerCase() || data.discriminator === args[0] || data.id === args[0]) || message.author,
-            linkavatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 }),
-            avatar = await message.guild.members.cache.get(user.id) ? await message.guild.members.cache.get(user.id)?.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }) : client.users.cache.get(user.id)?.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }),
-            Emojis = ['❌', '📨', '💙'],
-            embed = new MessageEmbed()
-                .setColor('#246FE0')
-                .setDescription(`[Clique aqui](${linkavatar}) para baixar o avatar de ${user.tag}`)
-                .setImage(avatar),
-            msg = await message.reply({ embeds: [embed] })
+            member = message.guild.members.cache.get(user.id)
 
-        return like()
+        let userAvatarURL = user.avatarURL({ dynamic: true, format: "png", size: 1024 }),
+            memberAvatarURL = member?.avatarURL({ dynamic: true, format: "png", size: 1024 }),
+            userAvatarImage = user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }),
+            memberAvatarImage = member.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }),
+            Emojis = ['⬅️', '📨', '🗑️', '💙', '➡️'],
+            embeds = [
+                new MessageEmbed()
+                    .setColor('#246FE0')
+                    .setDescription(`${e.Download} [Clique aqui](${userAvatarURL}) para baixar o avatar de ${user.tag}`)
+                    .setImage(userAvatarImage),
+                new MessageEmbed()
+                    .setColor('#246FE0')
+                    .setDescription(`${e.Download} [Clique aqui](${memberAvatarURL}) para baixar o avatar de ${user.tag}`)
+                    .setImage(memberAvatarImage)
+            ],
+            atualEmbed = 0, DmUser = []
 
-        async function like() {
-
-            let Delete = false,
-                DmUser = []
-
-            for (const emoji of Emojis)
-                msg.react(emoji).catch(() => { })
-
-            const Collector = msg.createReactionCollector({
-                filter: (reaction, u) => Emojis.includes(reaction.emoji.name) && u.id !== client.user.id,
-                time: 30000
-            })
-
-                .on('collect', (reaction, u) => {
-
-                    if (reaction.emoji.name === '❌' && (u.id === message.author.id || u.id === user.id)) {
-                        Delete = true
-                        return Collector.stop()
+        const buttonsWithArrows = [
+            {
+                type: 1,
+                components: [
+                    {
+                        type: 2,
+                        emoji: Emojis[0], // Left Arrow
+                        custom_id: 'leftArrow',
+                        style: 'PRIMARY'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[1], // letter
+                        custom_id: 'letter',
+                        style: 'PRIMARY'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[2], // X
+                        custom_id: 'x',
+                        style: 'DANGER'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[3], // Blue Heart
+                        custom_id: 'blueHeart',
+                        style: 'PRIMARY'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[4], // Right Arrow
+                        custom_id: 'rightArrow',
+                        style: 'PRIMARY'
                     }
+                ]
+            }
+        ]
 
-                    if (reaction.emoji.name === '📨') {
-
-                        if (DmUser.includes(u.id)) return
-
-                        u.send({ embeds: [embed.setFooter({ text: `Foto enviada de: ${message.guild.name}` })] }).catch(() => {
-                            return message.channel.send(`${e.Deny} | ${u}, sua DM está fechada. Verifique suas configurações e tente novamente.`)
-                        })
-                        DmUser.push(u.id)
-                        return message.channel.send(`${e.Check} | ${u} solicitou a foto de ${user.username} para sua DM.`)
-
+        const buttonsWithoutArrows = [
+            {
+                type: 1,
+                components: [
+                    {
+                        type: 2,
+                        emoji: Emojis[1], // Letter
+                        custom_id: 'letter',
+                        style: 'PRIMARY'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[2], // X
+                        custom_id: 'x',
+                        style: 'DANGER'
+                    },
+                    {
+                        type: 2,
+                        emoji: Emojis[3], // Blue Heart
+                        custom_id: 'blueHeart',
+                        style: 'PRIMARY'
                     }
+                ]
+            }
+        ]
 
-                    if (reaction.emoji.name === '💙') return NewLike(u)
-                    return
+        let msg = memberAvatarURL
+            ? await message.reply({ embeds: [embeds[0]], components: buttonsWithArrows })
+            : await message.reply({ embeds: [embeds[0]], components: buttonsWithoutArrows })
 
-                })
+        return msg.createMessageComponentCollector({
+            filter: int => true,
+            idle: 60000
+        })
+            .on('collect', (interaction) => {
+                interaction.deferUpdate().catch(() => { })
 
-                .on('end', () => {
+                let intId = interaction.customId,
+                    intUser = interaction.user
 
-                    if (Delete) {
-                        msg.delete(() => message.channel.send(`${e.Warn} | Ocorreu um erro ao excluir a mensagem.\n\`${err}\``))
-                        return message.delete().catch(err => message.channel.send(`${e.Warn} | Ocorreu um erro ao excluir a mensagem de origem.\n\`${err}\``))
-                    }
+                if (['leftArrow', 'rightArrow'].includes(intId) && intUser.id === message.author.id) {
+                    atualEmbed = atualEmbed === 0 ? 1 : 0
 
-                    return msg.edit({ embeds: [embed.setColor('RED').setFooter({ text: 'Tempo expirado.' })] }).catch(() => { })
-
-                })
-
-            async function NewLike(Author) {
-                if (user.id === client.user.id) return message.channel.send(`${Author}, olha... Eu agradeço... Mas você já viu meu \`${prefix}perfil @${client.user.username}\`?`)
-                if (Author.id === user.id || user.bot) return
-
-                let authorData = await Database.User.findOne({ id: Author.id }, 'Timeouts.Rep'),
-                    userData = await Database.User.findOne({ id: user.id })
-
-                if (!userData) {
-
-                    let u = client.users.cache.get(Author.id)
-
-                    if (!u)
-                        return message.reply(`${e.Deny} | Usuário desconhecido.`)
-
-                    Database.registerUser(u)
-                    return message.reply(`${e.Deny} | <@${Author.id}>, tenta de novo por favor...`)
+                    return msg.edit({ embeds: [embeds[atualEmbed]] }).catch(() => { })
                 }
 
-                if (client.Timeout(1800000, authorData.Timeouts.Rep))
-                    return message.channel.send(`${e.Nagatoro} | ${Author}, calminha aí Princesa! \`${client.GetTimeout(1800000, authorData.Timeouts.Rep)}\``)
+                if (intId === 'blueHeart')
+                    return NewLike(intUser)
 
-                Database.addItem(user.id, 'Likes', 1)
-                Database.SetTimeout(Author.id, 'Timeouts.Rep')
+                if (intId === 'x' && [message.author.id, user.id].includes(intUser.id)) {
+                    message.delete().catch(() => { })
+                    return msg.delete().catch(() => { })
+                }
 
-                return message.channel.send(`${e.Check} | ${Author} deu um like para ${user.tag}.`)
-            }
+                if (intId === 'letter')
+                    return sendLetter(intUser)
+
+                return
+            })
+            .on('end', () => msg.edit({ components: [] }).catch(() => { }))
+
+        function sendLetter(u) {
+
+            if (DmUser.includes(u.id)) return
+
+            u.send({ embeds: [embeds[atualEmbed].setFooter({ text: `Foto enviada de: ${message.guild.name}` })], components: [] }).catch(() => {
+                return message.channel.send(`${e.Deny} | ${u}, sua DM está fechada. Verifique suas configurações e tente novamente.`)
+            })
+            DmUser.push(u.id)
+            return message.channel.send(`${e.Check} | ${u} solicitou a foto de ${user.username} para sua DM.`)
+
         }
+
+        async function NewLike(Author) {
+            if (user.id === client.user.id) return message.channel.send(`${Author}, olha... Eu agradeço... Mas você já viu meu \`${prefix}perfil @${client.user.username}\`?`)
+            if (Author.id === user.id) return
+            if (user.bot) return message.channel.send(`${e.Deny} | <@${Author.id}>, bots não podem receber likes, ok?`)
+
+            let authorData = await Database.User.findOne({ id: Author.id }, 'Timeouts.Rep'),
+                userData = await Database.User.findOne({ id: user.id })
+
+            if (!userData) {
+
+                let u = client.users.cache.get(user.id)
+
+                if (!u)
+                    return message.reply(`${e.Deny} | Usuário desconhecido.`)
+
+                Database.registerUser(u)
+                return message.reply(`${e.Deny} | <@${Author.id}>, tenta de novo por favor...`)
+            }
+
+            if (client.Timeout(1800000, authorData.Timeouts.Rep))
+                return message.channel.send(`${e.Nagatoro} | ${Author}, calminha aí Princesa! \`${client.GetTimeout(1800000, authorData.Timeouts.Rep)}\``)
+
+            Database.addItem(user.id, 'Likes', 1)
+            Database.SetTimeout(Author.id, 'Timeouts.Rep')
+
+            return message.channel.send(`${e.Check} | ${Author} deu um like para ${user.tag}.`)
+        }
+
     }
 }
